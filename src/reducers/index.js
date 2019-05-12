fb2f84cd45247752895583510f060a56557b6af0
hirakiryuta <hirakiryuta@team-lab.com>
2019-05-12 21:39:58 +0900

    基本的なdirectory構成した、これからreadmeに追記

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux"
 import { reducer as form } from "redux-form"
 
+import toBuyListReducer from './toBuyListReducer'
+
 /** todo
  * reducerを作ったらここに追加
 */
 
-export default combineReducers({ form })
+export default combineReducers({ toBuyListReducer, form })
